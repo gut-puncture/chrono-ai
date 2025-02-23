25405dce46b253626cff8b80c18cbea171c6c574
Shailesh <75851986+gut-puncture@users.noreply.github.com>
2025-02-23 05:38:48 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 // pages/index.js
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Button, Container, Typography } from "@mui/material";
+import Link from "next/link";
 
 export default function Home() {
   const { data: session } = useSession();
@@ -11,9 +12,21 @@ export default function Home() {
         <Typography variant="h4" gutterBottom>
           Welcome, {session.user.name}
         </Typography>
-        <Button variant="contained" color="primary" onClick={() => signOut()}>
+        <Button 
+          variant="contained" 
+          color="primary" 
+          onClick={() => signOut()}
+          sx={{ mr: 2 }}
+        >
           Sign Out
         </Button>
+        
+        {/* NEW: Button or link to go to the chat page */}
+        <Link href="/chat" passHref>
+          <Button variant="contained" color="secondary">
+            Go to Chat
+          </Button>
+        </Link>
       </Container>
     );
   }
